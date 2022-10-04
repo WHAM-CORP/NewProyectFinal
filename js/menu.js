f14ba29483a3be26125d0f5eead743c2d13ccd5c
WHAM <williamsalbornozmendez9@gmail.com>
2022-10-03 20:22:37 -0500

    HeaderMenuBanner

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+var btnMenu = document.getElementById('btnmenu');
+var menu = document.getElementById('menu');
+btnmenu.addEventListener('click',function(){
+'use strict';
+menu.classList.toggle('mostrar');
+});
